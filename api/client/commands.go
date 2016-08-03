91f39d94964ed7aac8459428f14138394239f38e
Amit Kumar Das <amit.das@cloudbyte.com>
2016-08-03 10:38:26 +0530

    Adding comments to make this syntax well understood by all

--- api/client/commands.go
+++ api/client/commands.go
@@ -2,8 +2,8 @@ package client
 
 // Command returns a cli command handler if one exists
 func (cli *OpenEBSCli) Command(name string) func(...string) error {
-	// Names of commands are keys in this map
-	// The command handlers i.e. functions are the values in this map
+	// Names of commands are keys (of type string) in this map
+	// The command handlers are values (of type func(...string) error) in this map
 	return map[string]func(...string) error{
 	}[name]
 }
