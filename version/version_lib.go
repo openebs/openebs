69f9f523d1c472cab000d0541b2a0e489fe63baa
kmova <kiran.mova@cloudbyte.com>
2016-08-03 22:49:00 +0530

    Add script to auto-generate the version information

--- version/version_lib.go
+++ version/version_lib.go
@@ -6,8 +6,8 @@ package version
 // Default build-time variable for library-import.
 // This file is overridden on build with build-time informations.
 const (
-	GitCommit string = "library-import"
-	Version   string = "library-import"
-	BuildTime string = "library-import"
-	IAmStatic string = "library-import"
+	GitCommit string = "REPGITCOMMIT"
+	Version   string = "1.0"
+	BuildTime string = "REPBUILDTIME"
+	IAmStatic string = "true"
 )
