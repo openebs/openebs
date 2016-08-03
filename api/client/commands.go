7d1edf4e9bb0e3f677c21b163750ca15216f336d
Amit Kumar Das <amit.das@cloudbyte.com>
2016-08-03 10:36:22 +0530

    Added comments to this strange looking syntax
    The strange looking syntax may be idiomatic to go but we want all everyone to understand this well !!!

--- api/client/commands.go
+++ api/client/commands.go
@@ -2,6 +2,8 @@ package client
 
 // Command returns a cli command handler if one exists
 func (cli *OpenEBSCli) Command(name string) func(...string) error {
+	// Names of commands are keys in this map
+	// The command handlers i.e. functions are the values in this map
 	return map[string]func(...string) error{
 	}[name]
 }
