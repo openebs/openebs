6d680b667a01490ed750e11774f6de212e0bcdad
kmova <kiran.mova@cloudbyte.com>
2017-05-17 13:46:39 +0530

    Set the domain name to generic - domain.name

--- k8s/lib/vagrant/boxes/ubuntu-xenial/prepare_network.sh
+++ k8s/lib/vagrant/boxes/ubuntu-xenial/prepare_network.sh
@@ -4,5 +4,6 @@
 # /etc/resolv.conf is set with nameserver as 10.0.2.3
 # Change this to 127.0.0.1
 
-sudo sed -i "s/10\.0\.2\.3/127\.0\.0\.1/g" /etc/resolv.conf
+sudo sed -i "s/10\.0\.2\.3/8\.8\.8\.8/g" /etc/resolv.conf
+sudo sed -i "s/cbblr\.com/domain\.name/g" /etc/resolv.conf
 
