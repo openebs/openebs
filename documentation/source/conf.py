c5a825940ccaf736202d0a883240cde184e8c969
kmova <kiran.mova@cloudbyte.com>
2017-07-18 11:46:54 +0530

    #157 - customize sphinx_rtd_theme through override files

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -32,11 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinx.ext.autodoc',
-	#'sphinx.ext.autodoc',
-
-	#Add theme as extension so sitemap.xml is generated
-	#'sphinx_rtd_theme'
+	'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -95,14 +91,14 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'base_url': "http://openebs.readthedocs.io/en/",
-    'canonical_url': "http://openebs.readthedocs.io/en/"
-}
+#html_theme_options = {
+#    'base_url': "http://openebs.readthedocs.io/en/",
+#    'canonical_url': "http://openebs.readthedocs.io/en/"
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
-html_theme_path = ["_themes", ]
+#html_theme_path = ["_themes", ]
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -184,6 +180,4 @@ texinfo_documents = [
      author, 'openebs', 'Containerized storage for Containers',
      'Miscellaneous'),
 ]
-
-
 
