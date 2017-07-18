6d62a97188a47c1cc1de368e4bb825b6b2019485
kmova <kiran.mova@cloudbyte.com>
2017-07-18 11:50:06 +0530

    #157 - remove the layout used for older template

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -36,7 +36,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
