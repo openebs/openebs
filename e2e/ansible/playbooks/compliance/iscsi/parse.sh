8d1e9ac1b7704bc4db205a645606d003686dc496
ksatchit <karthik.s@cloudbyte.com>
2017-07-29 23:13:51 +0530

    Added support for running iSCSI compliance in CI via libiscsi test suite ------------------------------------------------------------------------ - Created playbook with associated task files (vars, prereq, cleanup),   scripts and input (test) data to run libiscsi test suite on the OpenEBS   storage volume as part of iSCSI compliance validation.
      (See https://github.com/sahlberg/libiscsi/blob/master/README)
    
    - The playbook performs the following functions :
    
      - Installs libiscsi on the kube minion (libiscsi-prerequisites.yml)
      - Creates a persistent volume as iSCSI target (pvc.yml)
      - Discovers the target and determines iqn via iscsi-ls (libiscsi.yml)
      - Executes the tests using iscsi-test-cu suite against the target
        (libiscsi.yml)from a test-suite list (run-tests.out)
      - Places logs for each suite in libiscsi_suite_logs (libiscsi.yml)
      - Parses the logs and generates test summary report SUMMARY.log and
        displays total number of passed and failed tests. (parse.sh)
      - Cleans-up the configuration (libiscsi-cleanup.yml)
      - Notifies user about test run (libiscsi.yml)
    
    - In the libiscsi suite, tests related to removable device (USB) has
      been omitted.

---
+++ e2e/ansible/playbooks/compliance/iscsi/parse.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+########################################################
+# This shell script parses the individual libiscsi test 
+# logs to generate a readable result summary file called
+# SUMMARY.log The script also displays the total no of 
+# passed and failed tests
+#
+# It takes the libiscsi test log folder path as argument
+########################################################
+
+Header()
+{
+file=$1
+testSuite=$2
+printf "###############################################\n" >> $file
+printf "\n" >> $file 
+printf "SUITE : $testSuite\n" >> $file
+printf "\n" >> $file 
+}
+
+log_location=$1
+result_file=`echo $HOME/SUMMARY.log`
+
+> $result_file
+
+t_p_count=0
+t_f_count=0
+
+for i in `ls $log_location | grep .log` 
+do
+{
+ suite=`echo $i | sed s/'.log'//g`
+ 
+ Header $result_file $suite;
+ 
+ p_c=`cat $log_location/$i | grep Test | grep -i passed | wc -l` 
+ t_p_count=`expr $t_p_count + $p_c`
+ cat $log_location/$i | grep Test | grep -i passed | awk '{printf("%-5s %20s %10s\n", $1, $2, "PASSED")}' >> $result_file
+ 
+ f_c=`cat $log_location/$i | grep Test | grep -i failed | wc -l`
+ t_f_count=`expr $t_f_count + $f_c`
+ cat $log_location/$i | grep Test | grep -i failed | awk '{printf("%-5s %20s %10s\n", $1, $2, "FAILED")}' >> $result_file 
+}
+done
+ 
+printf "No of tests passed:%s  No of tests failed:%s" $t_p_count $t_f_count
+
+
+
