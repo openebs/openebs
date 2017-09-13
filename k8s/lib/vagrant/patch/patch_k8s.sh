8164a4aafe691d3b366267f12cea74ad213c0ddd
kmova <kiran.mova@cloudbyte.com>
2017-09-13 10:08:54 +0530

    Framework for patching existing vagrant boxes
    There are scenarios where, once an box is uploaded,
    the box needs to be updated with either files or
    packages.
    
    In this case, the configure_k8s_weave.sh was required
    to be updated.
    
    This commit provides an initial framework using which
    the box can be upgraded. This commit hard-codes the
    version of the box to be patched and the patch code
    itself. There will be enhancment requests raised to
    make this more generic.

---
+++ k8s/lib/vagrant/patch/patch_k8s.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Insert the patch instructions here
+
+# Patch 01 - Copy the latest configure_k8s_weave.sh 
+# Location of the k8s configure scripts
+scriptloc="/vagrant/workdir/scripts/k8s"
+cd /home/ubuntu/setup/k8s
+cp ${scriptloc}/configure_k8s_weave.sh .
+# END Patch 01
+
+
+# DONOT MODIFY BELOW THIS LINE
+# Cleaning up apt and bash history before packaing the box. 
+sudo mkdir -p /etc/systemd/system/apt-daily.timer.d/
+cat <<EOF | sudo tee -a /etc/systemd/system/apt-daily.timer.d/apt-daily.timer.conf > /dev/null
+[Timer]
+Persistent=false
+EOF
+
+sudo apt-get clean
+cat /dev/null > ~/.bash_history && history -c && exit
