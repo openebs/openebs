57297239bedb137970f477504bca901987ced93f
Harshit <harshit@esthenos.com>
2017-10-21 12:44:12 +0530

    Fix | PEP8 | Rename generate-inventory.py to generate_inventory.py in openebs/e2e/ansible/roles/inventory/files

--- e2e/ansible/roles/inventory/files/generate_inventory.py
+++ e2e/ansible/roles/inventory/files/generate_inventory.py
@@ -17,11 +17,11 @@
 #
 # Usage Examples:
 #
-# python generate-inventory.py <path>/machines.in
+# python generate_inventory.py <path>/machines.in
 #
-# python generate-inventory.py <path>/machines.in --loglevel=Debug
+# python generate_inventory.py <path>/machines.in --loglevel=Debug
 #
-# python generate-inventory.py <path>/machines.in --loglevel=Info \
+# python generate_inventory.py <path>/machines.in --loglevel=Info \
 #       --passwordless=True
 #
 ######################################################################
