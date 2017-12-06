7d149b39ab02be426992dd428896e4ef06a49263
yudaykiran <udaykiran.y@gmail.com>
2017-12-06 16:06:07 +0530

    Changes Include: - Only install one package of a file even if multiple packages are downloaded. - Add version to the fetch script and Vagrantfile to install the correct package.
    Signed-off-by: yudaykiran <udaykiran.y@gmail.com>

--- k8s/lib/vagrant/boxes/k8s/fetch_kubeadm.sh
+++ k8s/lib/vagrant/boxes/k8s/fetch_kubeadm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+kubeversion=${1:-"1.7.5"}
 #Update the repository index
 apt-get update && apt-get install -y apt-transport-https 
 
@@ -7,6 +7,6 @@ apt-get update && apt-get install -y apt-transport-https
 apt-get install -y docker.io socat ebtables
 
 sudo dpkg -i /vagrant/workdir/dpkgs/kubernetes-cni*.deb
-sudo dpkg -i /vagrant/workdir/dpkgs/kubelet*.deb
-sudo dpkg -i /vagrant/workdir/dpkgs/kubectl*.deb
-sudo dpkg -i /vagrant/workdir/dpkgs/kubeadm*.deb
+sudo dpkg -i /vagrant/workdir/dpkgs/kubelet_${kubeversion}-00*.deb
+sudo dpkg -i /vagrant/workdir/dpkgs/kubectl_${kubeversion}-00*.deb
+sudo dpkg -i /vagrant/workdir/dpkgs/kubeadm_${kubeversion}-00*.deb
