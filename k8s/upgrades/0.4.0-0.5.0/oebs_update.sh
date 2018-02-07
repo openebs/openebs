b4a39cc4a5aee05a205380d35afd66e97fd67f24
ksatchit <karthik.s@cloudbyte.com>
2018-02-07 13:04:47 +0530

    Comment additions and patch file renames ---------------------------------------- Updates to oebs_update.sh to add comments Rename patch files as *.patch.tpl.yml to signify templates
    Signed-off-by: ksatchit <karthik.s@cloudbyte.com>

--- k8s/upgrades/0.4.0-0.5.0/oebs_update.sh
+++ k8s/upgrades/0.4.0-0.5.0/oebs_update.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-#### GET PV NAME AS ARGUMENT #### 
+################################################################
+# STEP: Get Persistent Volume (PV) name as argument            #
+#                                                              #
+# NOTES: Obtain the pv to upgrade via "kubectl get pv"         #
+################################################################
+
 pv=$1
 
-#### GENERATE DEPLOY, REPLICASET AND CONTAINER NAMES FROM PV ####
+################################################################ 
+# STEP: Generate deploy, replicaset and container names from PV#
+#                                                              #
+# NOTES: Ex: If PV="pvc-cec8e86d-0bcc-11e8-be1c-000c298ff5fc", #
+#                                                              #
+# ctrl-dep: pvc-cec8e86d-0bcc-11e8-be1c-000c298ff5fc-ctrl      #  
+# ctrl-cont: pvc-cec8e86d-0bcc-11e8-be1c-000c298ff5fc-ctrl-con #  
+################################################################
+
 c_dep=$(echo $pv-ctrl); c_name=$(echo $c_dep-con)
 r_dep=$(echo $pv-rep); r_name=$(echo $r_dep-con)
 
 c_rs=$(kubectl get rs -o name | grep $c_dep | cut -d '/' -f 2)
 
-#### EDIT PATCH FILES WITH APPROPRIATE CONTAINER NAMES ####
-sed -i "s/pvc[^ \"]*/$r_name/g" replica.patch.yml
-sed -i "s/pvc[^ \"]*/$c_name/g" controller.patch.yml
+################################################################ 
+# STEP: Update patch files with appropriate container names    #
+#                                                              # 
+# NOTES: Placeholder "pvc-<deploy-hash>-ctrl/rep-con in the    #
+# patch files are replaced with container names derived from   #
+# the PV in the previous step                                  #  
+################################################################
 
-#### PATCH JIVA REPLICA DEPLOYMENT ####
-kubectl patch deployment $r_dep -p "$(cat replica.patch.yml)"
+sed -i "s/pvc[^ \"]*/$r_name/g" replica.patch.tpl.yml
+sed -i "s/pvc[^ \"]*/$c_name/g" controller.patch.tpl.yml
+
+################################################################
+# STEP: Patch OpenEBS volume deployments (controller, replica) #  
+#                                                              #
+# NOTES: Strategic merge patch is used to update the volume w/ #  
+# rollout status verification                                  #  
+################################################################
+
+# PATCH JIVA REPLICA DEPLOYMENT ####
+kubectl patch deployment $r_dep -p "$(cat replica.patch.tpl.yml)"
 rc=$?; if [ $rc -ne 0 ]; then echo "ERROR: $rc"; exit; fi
 
 rollout_status=$(kubectl rollout status deployment/$r_dep)
@@ -22,14 +49,19 @@ rc=$?; if [[ ($rc -ne 0) || !($rollout_status =~ "successfully rolled out") ]];
 then echo "ERROR: $rc"; exit; fi
 
 #### PATCH CONTROLLER DEPLOYMENT ####
-kubectl patch deployment $c_dep -p "$(cat controller.patch.yml)"
+kubectl patch deployment $c_dep -p "$(cat controller.patch.tpl.yml)"
 rc=$?; if [ $rc -ne 0 ]; then echo "ERROR: $rc"; exit; fi
 
 rollout_status=$(kubectl rollout status deployment/$c_dep)
 rc=$?; if [[ ($rc -ne 0) || !($rollout_status =~ "successfully rolled out") ]];
 then echo "ERROR: $rc"; exit; fi
 
-#### REMOVE STALE CONTROLLER REPLICASET #### 
+################################################################
+# STEP: Remove Stale Controller Replicaset                     #
+#                                                              # 
+# NOTES: This step is applicable upon label selector updates,  #
+# where the deployment creates orphaned replicasets            #
+################################################################
 kubectl delete rs $c_rs 
 
 
