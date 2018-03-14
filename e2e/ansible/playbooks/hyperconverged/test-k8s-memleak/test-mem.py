47b67a936825abd7ceccebd20cdbc0ac2153da95
Sudarshan Darga <sudarshan.darga@cloudbyte.com>
2018-03-14 16:27:18 +0530

    Comments added

--- e2e/ansible/playbooks/hyperconverged/test-k8s-memleak/test-mem.py
+++ e2e/ansible/playbooks/hyperconverged/test-k8s-memleak/test-mem.py
@@ -1,16 +1,18 @@
+#------------------------------------------------------------------------------
+#!/usr/bin/env python
+#description     :Test to verify the memory consumed with sample workloads. 
+#==============================================================================
+
 from __future__ import division
 import subprocess
 import time, os
-
-cmd_total_mem = "sudo free | awk ' /'Mem'/ {print $2}'"
-
-out = subprocess.Popen(cmd_total_mem,stdout=subprocess.PIPE, shell=True)
-total_mem = out.communicate()
 list = []
 n = 10
 count = 0
 while count < n:
     count = count + 1
+    #Obtaining memory consumed using free command from the node where the controller
+    #is scheduled.
     cmd_used_mem = "sudo free | awk ' /'Mem'/ {print $3}'"
     out = subprocess.Popen(cmd_used_mem,stdout=subprocess.PIPE, shell=True)
     used_mem = out.communicate()
@@ -18,6 +20,9 @@ while count < n:
     time.sleep(20)
     list.append(mem_in_mb)
 print list
+# A watermark of 500MB has been set based on benchmark runs with the workload 
+# profile chosen in this test
+# TODO: Identify better mem consumption strategies
 if all(i <= 500 for i in list):
         print "Pass"
 else:
