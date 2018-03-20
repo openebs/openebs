004d842a47752f7de2018810105083c6741e43ae
Sudarshan Darga <sudarshan.darga@cloudbyte.com>
2018-03-20 12:48:30 +0530

    Adding workload characteristics to memleak-vars.yml
    Signed-off-by: Sudarshan Darga <sudarshan.darga@cloudbyte.com>

--- e2e/ansible/playbooks/hyperconverged/test-k8s-memleak/test-mem.py
+++ e2e/ansible/playbooks/hyperconverged/test-k8s-memleak/test-mem.py
@@ -5,10 +5,10 @@
 
 from __future__ import division
 import subprocess
-import time, os
+import time, os, sys
 list = []
 cmd_cntrl_name = "kubectl get pod -l openebs/controller=jiva-controller --no-headers | awk '{print $1}'"
-out = subprocess.Popen(cmd_cntrl_name,stdout=subprocess.PIPE, shell=True)
+out = subprocess.Popen(cmd_cntrl_name,stdout=subprocess.PIPE,shell=True)
 cntrl_name = out.communicate()
 cntrl_pod_name = cntrl_name[0].strip('\n')
 n = cntrl_pod_name.split('-')
@@ -16,16 +16,22 @@ lst = n[:len(n)-2]
 lst.append("con")
 container_name = "-".join(lst)
 used_mem_process = "kubectl exec %s -c %s -- pmap -x 1 | awk ''/total'/ {print $3}'" %(cntrl_pod_name,container_name)
+print used_mem_process
 n = 5
 count = 0
 #Obtaining memory consumed by longhorn process from the cntroller pod.
 while count < n:
     count = count + 1
-    out = subprocess.Popen(used_mem_process,stdout=subprocess.PIPE, shell=True)
+    out = subprocess.Popen(used_mem_process,stdout=subprocess.PIPE,shell=True)
     used_mem = out.communicate()
     mem_in_mb = int(used_mem[0])/1024
     print mem_in_mb, "MB"
-    time.sleep(20)
+    if mem_in_mb < 500:
+        time.sleep(20)
+    else:
+        print "Fail"
+        #break
+        quit()
     list.append(mem_in_mb)
 print list
 # A watermark of 500MB has been set based on benchmark runs with the workload 
@@ -35,5 +41,4 @@ if all(i <= 500 for i in list):
         print "Pass"
 else:
         print "Fail"
-
 
