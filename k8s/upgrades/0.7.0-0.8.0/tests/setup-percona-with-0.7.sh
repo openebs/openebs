95ac0c67aa62f2db753f0e682cd52c3b1d7ca5ce
kmova <kiran.mova@openebs.io>
2018-11-26 10:52:34 +0000

    feat(install): upgrade scripts, yamls for 0.8.0
    - Created upgrade scripts for 0.7.x to 0.8.0
      - Create patch files for jiva, cstor volumes
      - Create patch files for cStor pools
      - Update instructions
    - Update the openebs-operator.yaml with new ENV
      for analytics
    - Remove the unused maya-apiserver install configmap
    - Update the charts with 0.8.0 changes
    - Also includes some minor updates to the demo YAML files
    
    Signed-off-by: kmova <kiran.mova@openebs.io>

---
+++ k8s/upgrades/0.7.0-0.8.0/tests/setup-percona-with-0.7.sh
@@ -0,0 +1,26 @@
+kubectl apply -f https://openebs.github.io/charts/openebs-operator-0.7.2.yaml
+
+echo "Waiting for m-apiserver to be ready"
+JSONPATH='{range .items[0]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; 
+until kubectl get pods -n openebs -l name=maya-apiserver -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True";
+do
+  echo -n "."
+  sleep 2; 
+done
+echo ""
+kubectl get pods -n openebs
+
+echo "Launching percona with jiva"
+kubectl apply -f https://raw.githubusercontent.com/openebs/openebs/master/k8s/demo/percona/percona-openebs-deployment.yaml
+
+JSONPATH='{range .items[0]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; 
+until kubectl get pods -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True";
+do
+  echo -n "."
+  sleep 2; 
+done
+echo ""
+kubectl get pods 
+
+echo "Launching percona with cstor"
+kubectl apply -f https://raw.githubusercontent.com/openebs/openebs/master/k8s/demo/percona/percona-openebs-cstor-sparse-deployment.yaml
