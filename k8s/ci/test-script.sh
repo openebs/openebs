46cf2e2fa3ee7ddedbab52492a7d1054850be50a
princerachit <prince.rachit@mayadata.io>
2018-12-03 15:34:05 +0530

    remove unused env variable
    Signed-off-by: princerachit <prince.rachit@mayadata.io>

--- k8s/ci/test-script.sh
+++ k8s/ci/test-script.sh
@@ -122,7 +122,6 @@ waitForDeployment ${CSTORTARGET} default
 MAPI_SVC_ADDR=`kubectl get service -n openebs maya-apiserver-service -o json | grep clusterIP | awk -F\" '{print $4}'`
 export MAPI_ADDR="http://${MAPI_SVC_ADDR}:5656"
 export KUBERNETES_SERVICE_HOST="127.0.0.1"
-export MAYACTL="$GOPATH/src/github.com/openebs/maya/bin/maya/mayactl"
 export KUBECONFIG=$HOME/.kube/config
 
 
