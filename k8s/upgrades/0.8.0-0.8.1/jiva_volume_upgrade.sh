fc57975f1d2eb8627f065241ee89205c0bca567e
Ashish Ranjan <ashishranjan738@gmail.com>
2019-01-30 00:27:43 +0530

    refactor(upgrade): Removes set -e flag
    Signed-off-by: Ashish Ranjan <ashishranjan738@gmail.com>
    
    This commit removes set -e flag from upgrade scripts as error is handled
    in most cases

--- k8s/upgrades/0.8.0-0.8.1/jiva_volume_upgrade.sh
+++ k8s/upgrades/0.8.0-0.8.1/jiva_volume_upgrade.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 ################################################################
 # STEP: Get Persistent Volume (PV) name as argument            #
 #                                                              #
