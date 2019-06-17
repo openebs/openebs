54259607579b2119af9baa74771fce012b7f12be
mittachaitu <sai.chaithanya@mayadata.io>
2019-06-17 20:58:11 +0530

    This commit updates the scripts to change the OpenEBS cStor pools and volumes from 0.9.0 to 1.0.0
    Signed-off-by: mittachaitu <sai.chaithanya@mayadata.io>

--- k8s/upgrades/0.9.0-1.0.0/cstor_pool_upgrade.sh
+++ k8s/upgrades/0.9.0-1.0.0/cstor_pool_upgrade.sh
@@ -6,12 +6,7 @@
 # NOTES: Obtain the pool deployments to perform upgrade operation         #
 ###########################################################################
 
-##TODO:
-##1) Need to Update the function names
-##2) verify the return status of all the kubectl commands
-##3) code comments will be updated
-
-pool_upgrade_version="0.1.0"
+pool_upgrade_version="v1.0.x-ci"
 current_version="0.9.0"
 
 function usage() {
@@ -32,8 +27,10 @@ function verify_openebs_version() {
     local name_res=$2
     local openebs_version=$(kubectl get $resource $name_res -n $ns \
                  -o jsonpath="{.metadata.labels.openebs\.io/version}")
+    rc=$?; if [ $rc -ne 0 ]; then echo "Failed to get version from $resource: $name_res Exit Code: $rc"; exit; fi
 
-    if [[ $openebs_version != $current_version ]] && [[ $openebs_version != $pool_upgrade_version ]]; then
+    if [[ $openebs_version != $current_version ]] && \
+          [[ $openebs_version != $pool_upgrade_version ]]; then
         echo "Expected version of $name_res in $resource is $current_version but got $openebs_version";exit 1;
     fi
     echo $openebs_version
@@ -54,61 +51,21 @@ function get_csp_list() {
     echo $csp_list
 }
 
-## insert_node_disk_list updates the map[nodeName]diskNames
-function insert_node_disk_list() {
-    local node_name=$1
-    local disk_name=$2
-    node_disklist["$node_name"]=$(echo "${node_disklist[$node_name]},$disk_name")
-}
-
-## make_map_node_disk_list get the disk info
-function make_map_node_disk_list() {
-    local disk_list=$1
-    local node_name=""
-    for disk_name in `echo $disk_list | tr "," " "`; do
-        node_name=$(kubectl get disk $disk_name -o jsonpath="{.metadata.labels.kubernetes\.io/hostname}")
-        insert_node_disk_list $node_name $disk_name
-    done
-}
-
-## get_disk_list_for_sp_list return the disk names present in given sp_list
-function get_disk_list_for_sp_list() {
-    local sp_list=$1
-    local disk_list=""
-    for sp in `echo $sp_list | tr ":" " "`; do
-        version=$(verify_openebs_version "sp" $sp)
-        rc=$?
-        if [ $rc -ne 0 ]; then
-            exit 1
-        elif [ $version == $pool_upgrade_version ]; then
-            continue
-        fi
-        local tmp_disk_list=$(kubectl get sp $sp -o jsonpath="{.spec.disks.diskList}")
-        tmp_disk_list=$(echo $tmp_disk_list | sed  's|\[||g; s|\]||g; s| |,|g')
-        if [ -z $disk_list ]; then
-            disk_list=$(echo $tmp_disk_list)
-        else
-            disk_list=$(echo $disk_list,$tmp_disk_list)
-        fi
-    done
-    echo $disk_list
-}
-
 ## patch_blockdevice_list_for_spc to patch block device changes related to spc
 function patch_blockdevice_list_for_spc() {
     local spc_name=$1
-    local sp_list=$2
-    local disk_list=$(kubectl get spc $spc_name \
-                      -o jsonpath='{.spec.disks.diskList}' | \
-                      sed  's|\[||g; s|\]||g; s| |,|g')
 
-    if [ -n "$disk_list" ]; then
-        disk_list=$(get_disk_list_for_sp_list $sp_list)
-    fi
-}
+    ## Get spc yaml and replace disk related info with block device
+    kubectl get spc $spc_name -o yaml | \
+                   sed 's|disks|blockDevices|g' | \
+                   sed 's|diskList|blockDeviceList|g' | \
+                   sed 's|disk-|blockdevice-|g' > spc-patch.json
+    rc=$?; if [ $rc -ne 0 ]; then echo "Failed to get spc: $spc_name yaml Exit Code: $rc"; exit; fi
 
-function make_spc_bd_list() {
-    local block_device_list=$1
+    kubectl apply -f spc-patch.json
+    rc=$?; if [ $rc -ne 0 ]; then echo "Failed to patch spc: $spc_name with block deivce info Exit Code: $rc"; exit; fi
+
+    rm spc-patch.json
 }
 
 ## make_csp_disk_list will return the jsonpath required to update the csp
@@ -134,12 +91,13 @@ fi
 
 spc=$1
 ns=$2
-declare -A node_disklist
+declare -A csp_blockdevice_list
 
 ### Get the deployment pods which are in not running state that are related to provided spc ###
 pending_pods=$(kubectl get po -n $ns \
     -l app=cstor-pool,openebs.io/storage-pool-claim=$spc \
     -o jsonpath='{.items[?(@.status.phase!="Running")].metadata.name}')
+rc=$?; if [ $rc -ne 0 ]; then echo "Failed to get pods related to spc: $spc Exit Code: $rc"; exit; fi
 
 
 ## If any deployments pods are in not running state then exit the upgrade process ###
@@ -148,18 +106,10 @@ if [ $(echo $pending_pods | wc -w) -ne 0 ]; then
     exit 1
 fi
 
+patch_blockdevice_list_for_spc $spc
+
 ### Get the csp list which are related to the given spc ###
 csp_list=$(get_csp_list $spc)
-
-### Get the sp list which are related to the given spc ###
-sp_list=$(kubectl get sp \
-          -l openebs.io/cas-type=cstor,openebs.io/storage-pool-claim=$spc \
-          -o jsonpath="{range .items[*]}{@.metadata.name}:{end}")
-rc=$?
-if [ $rc -ne 0 ]; then
-    echo "Failed to get sp related to spc $spc"
-    exit 1
-fi
 
 #### Get required info from current csp and use the info while upgrading ####
 for csp_name in `echo $csp_list | tr ":" " "`; do
@@ -176,35 +126,101 @@ for csp_name in `echo $csp_list | tr ":" " "`; do
     sp_name=$(kubectl get sp \
               -l openebs.io/cstor-pool=$csp_name \
               -o jsonpath="{.items[*].metadata.name}")
+    rc=$?; if [ $rc -ne 0 ]; then echo "Failed to get sp related to csp: $csp_name Exit Code: $rc"; exit; fi
 
     sp_disk_list=$(kubectl get sp $sp_name \
                    -o jsonpath="{.spec.disks.diskList}" | \
                     sed 's|\[||g; s|\]||g; s| |,|g')
+    rc=$?; if [ $rc -ne 0 ]; then echo "Failed to get disks related to sp: $sp_name Exit Code: $rc"; exit; fi
 
+    ## Below snippet will get related info regarding block device and format
+    ## the information in below format and save it with corresponding csp name
+    ## "group": [
+    ##    {
+    ##         "blockDevice": [
+    ##             {
+    ##                 "deviceID": "/var/openebs/sparse/6-ndm-sparse.img",
+    ##                 "inUseByPool": true,
+    ##                 "name": "sparse-177b6bc2ae2dd332c7a384a02179368b"
+    ##             },
+    ##             {
+    ##                 "deviceID": "/var/openebs/sparse/9-ndm-sparse.img",
+    ##                 "inUseByPool": true,
+    ##                 "name": "sparse-2239d60eb46b3c26b0428fae4d15c88a"
+    ##             }
+    ##         ]
+    ##    }
+    ## ]
     csp_disk_list="init"
     for disk_name in `echo $sp_disk_list | tr "," " "`; do
          ## Assuming device Id present in first index
          device_id=$(kubectl get disk $disk_name -o jsonpath="{.spec.devlinks[0].links[0]}")
-         if [ -z device_id ]; then
+         if [ -z $device_id ]; then
              device_id=$(kubectl get disk $disk_name -o jsonpath="{.spec.path}")
          fi
          csp_disk_list=$(make_csp_disk_list "$csp_disk_list" "$device_id" "$disk_name")
     done
-    sed "s|@pool_version@|$pool_upgrade_version|g" csp-patch.tpl.json | \
-                sed "s|@blockDevice_list@|$csp_disk_list|g" > csp-patch.json
+    csp_blockdevice_list[$csp_name]=$csp_disk_list
+    sed "s|@pool_version@|$pool_upgrade_version|g" csp-metadata-patch.tpl.json > csp-patch.json
 
     kubectl patch csp $csp_name -p "$(cat csp-patch.json)" --type=merge
     rc=$?; if [ $rc -ne 0 ]; then echo "Error occured while upgrading the csp: $csp_name Exit Code: $rc"; exit; fi
+
 done
 
 rm csp-patch.json
 
-### Make node_disklist contains map of nodeName & disks attached to corresponding node
-get_disk_list_for_spc $spc $sp_list
+echo "Patching Pool Deployment with new image"
+for csp_name in `echo $csp_list | tr ":" " "`; do
+    ## Get the pool deployment corresponding to csp
+    pool_dep=$(kubectl get deploy -n $ns \
+        -l app=cstor-pool,openebs.io/storage-pool-claim=$spc \
+        -o jsonpath="{.items[?(@.metadata.labels.openebs\.io/cstor-pool=='$csp_name')].metadata.name}")
+    rc=$?; if [ $rc -ne 0 ]; then echo "Failed to get deployment related to csp: $csp_name Exit Code: $rc"; exit; fi
 
-##TODO: for testing added below snippet and it will be removed after removing WIP tag####
-echo "node disk list${!node_disklist[@]}"
+    version=$(verify_openebs_version "deploy" $pool_dep)
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        exit 1
+    elif [ $version == $pool_upgrade_version ]; then
+        continue
+    fi
 
-for key in `echo ${!node_disklist[@]}`; do
-    echo "$key - ${node_disklist[$key]}"
+    ## Get the replica set corresponding to the deployment ##
+    pool_rs=$(kubectl get rs -n $ns \
+        -o jsonpath="{range .items[?(@.metadata.ownerReferences[0].name=='$pool_dep')]}{@.metadata.name}{end}")
+    echo "$pool_dep -> rs is $pool_rs"
+
+
+    ## Modifies the cstor-pool-patch template with the original values ##
+    sed "s/@pool_version@/$pool_upgrade_version/g" cstor-pool-patch.tpl.json > cstor-pool-patch.json
+
+    ## Patch the deployment file ###
+    kubectl patch deployment --namespace $ns $pool_dep -p "$(cat cstor-pool-patch.json)"
+    rc=$?; if [ $rc -ne 0 ]; then echo "ERROR: Failed to patch $pool_dep $rc"; exit; fi
+    rollout_status=$(kubectl rollout status --namespace $ns deployment/$pool_dep)
+    rc=$?; if [[ ($rc -ne 0) || !($rollout_status =~ "successfully rolled out") ]];
+    then echo "ERROR: Failed to rollout status for $pool_dep error: $rc"; exit; fi
+
+    ## Deleting the old replica set corresponding to deployment
+    kubectl delete rs $pool_rs --namespace $ns
+
+    ## Remove the reconcile.openebs.io/disable annotation and patch with block
+    ## device information to csp
+    sed "s|@blockDevice_list@|${csp_blockdevice_list[$csp_name]}|g" csp-spec-patch.tpl.json > csp-spec-patch.json
+    kubectl patch csp $csp_name -p "$(cat csp-spec-patch.json)" --type=merge
+    rc=$?; if [ $rc -ne 0 ]; then echo "Failed to patch spec and annotation for csp: $csp Exit Code: $rc"; exit; fi
+
+    ## Cleaning the temporary patch file
+    rm cstor-pool-patch.json
+    rm csp-spec-patch.json
 done
+
+## Delete sp realated to the spc
+kubectl delete sp -l openebs.io/cas-type=cstor,openebs.io/storage-pool-claim=$spc
+rc=$?; if [ $rc -ne 0 ]; then echo "Failed to delete sp related to spc: $spc_name Exit Code: $rc"; exit; fi
+
+## Remove the reconcile.openebs.io/disable annotation from spc
+kubectl patch spc $spc -p "$(cat annotation-remove.json)" --type=merge
+
+echo "Successfully upgrade $spc to $pool_upgrade_version Please run volume upgrade scripts."
