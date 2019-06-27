5460cf98f6067b7db057b2e176f6d42ad8c5c5a0
shubham <shubham.bajpai@mayadata.io>
2019-06-27 21:08:19 +0530

    fix(upgrade) segregated verification steps from jiva volume upgrade script
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- k8s/upgrades/0.9.0-1.0.0/cstor/verify_pool_upgrade.sh
+++ k8s/upgrades/0.9.0-1.0.0/cstor/verify_pool_upgrade.sh
@@ -126,7 +126,7 @@ if [ $is_upgrade_failed == 0 ]; then
 else
     echo -n "Validation steps are failed on pool $spc. This might be"
     echo "due to ongoing upgrade or errors during upgrade."
-    echo -n "Please Re-run ./verify_pool_upgrade.sh <spc_name> <namespace> after "
+    echo -n "Please run ./verify_pool_upgrade.sh <spc_name> <namespace> again after "
     echo "some time. If issue still persist, contact OpenEBS team over slack for any further help."
     exit 1
 fi
