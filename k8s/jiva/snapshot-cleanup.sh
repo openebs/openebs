e80be67242a846c98547687ce6e94024510fb81f
shashank855 <shashank.ranjan@mayadata.io>
2019-07-04 12:10:14 +0530

    Change pvc_namespace to ctrl_namespace
    Signed-off-by: shashank855 <shashank.ranjan@mayadata.io>

--- k8s/jiva/snapshot-cleanup.sh
+++ k8s/jiva/snapshot-cleanup.sh
@@ -2,27 +2,27 @@
 
 usage()
 {
-    echo "Usage: bash script.sh <controller_pod_name> <application_namespace> <numbber_of_snapshots_to_delete>"
+    echo "Usage: bash script.sh <controller_pod_name> <pvc_namespace> <numbber_of_snapshots_to_delete>"
     exit 1
 }
 
 delete_jiva_snapshot()
 {
 ctrl_pod_name=$1
-application_namespace=$2
+ctrl_namespace=$2
 number_of_snapshot=$3
 count=0
-snapshot_list_cmd='kubectl exec -it $ctrl_pod_name -n $application_namespace -- jivactl snapshot ls | grep -v ID | wc -l'
-snapshot_name_cmd='kubectl exec -it $ctrl_pod_name -n $application_namespace -- jivactl snapshot ls | grep -v ID | awk 'NR==2''
+snapshot_list_cmd='kubectl exec -it $ctrl_pod_name -n $ctrl_namespace -- jivactl snapshot ls | grep -v ID | wc -l'
+snapshot_name_cmd='kubectl exec -it $ctrl_pod_name -n $ctrl_namespace -- jivactl snapshot ls | grep -v ID | awk 'NR==2''
 
-if [ $(eval $snapshot_list_cmd) -le $((number_of_snapshot)) ]; then
+if [ $(eval $snapshot_list_cmd) -le $((number_of_snapshot-1)) ]; then
  echo "Provided number of snapshots are not present"
 else
  block_service
- while [ $count -lt $number_of_snapshot ]
+ while [ $count -lt $((number_of_snapshot-1)) ]
  do 
    snapshot_name=$(eval $snapshot_name_cmd | tr -d '\r')
-   del_cmd="kubectl exec -it $ctrl_pod_name -n $application_namespace -- jivactl snapshot rm $snapshot_name"
+   del_cmd="kubectl exec -it $ctrl_pod_name -n $ctrl_namespace -- jivactl snapshot rm $snapshot_name"
    eval $del_cmd
    count=$((count+1))
    validate_snap_delete $snapshot_name
@@ -34,15 +34,15 @@ fi
 #This fuction is changeing the selector field the of controller service to block connection requests.
 block_service()
 {
-ctrl_service_name=$(kubectl get service -n $application_namespace -l openebs.io/controller-service=jiva-controller-svc -o jsonpath='{.items[0].metadata.name}')
-kubectl patch svc $ctrl_service_name -n $application_namespace --type merge -p "$(cat patch.json)" 
+ctrl_service_name=$(kubectl get service -n $ctrl_namespace -l openebs.io/controller-service=jiva-controller-svc -o jsonpath='{.items[0].metadata.name}')
+kubectl patch svc $ctrl_service_name -n $ctrl_namespace --type merge -p "$(cat patch.json)" 
 }
 
 #This fuction is restoring the selector field of the controller service.
 unblock_service()
 {
-kubectl patch svc $ctrl_service_name -n $application_namespace -p "{\"spec\":{\"selector\":{\"openebs.io/persistent-volume\": \"$pv_name\"}}}"
-kubectl patch svc $ctrl_service_name -n $application_namespace --type merge -p "$(cat patch.json)" 
+kubectl patch svc $ctrl_service_name -n $ctrl_namespace -p "{\"spec\":{\"selector\":{\"openebs.io/persistent-volume\": \"$pv_name\"}}}"
+kubectl patch svc $ctrl_service_name -n $ctrl_namespace --type merge -p "$(cat patch.json)" 
 sed -i 's/3260/3261/g' patch.json
 }
 
@@ -50,7 +50,7 @@ sed -i 's/3260/3261/g' patch.json
 validate_snap_delete()
 {
 del_snapshot_name=$1
-validate_cmd='kubectl exec -it $ctrl_pod_name -n $application_namespace -- jivactl snapshot ls | grep $del_snapshot_name'
+validate_cmd='kubectl exec -it $ctrl_pod_name -n $ctrl_namespace -- jivactl snapshot ls | grep $del_snapshot_name'
 eval $validate_cmd
 if [ $? == "0" ]; then
  echo "Unable to delete $del_snapshot_name"
