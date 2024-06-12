61cfa9a2a7937595d37ad49cf21fdd66d1e1a7fd
Orville Wright <orville.wright@yahoo.com>
2024-06-12 14:32:31 -0700

    chore (update metadata) date (#3740)
    Force update to file metadata date field
    
    Signed-off-by: Orville Wright <orville.wright@yahoo.com>

--- docs.sh
+++ docs.sh
@@ -7,7 +7,7 @@ git checkout -b docs-update
 sed -i 's/2.11.0/2.12.0/g' k8s/upgrades/README.md
 git status
 git diff
-read -p "Press enter to continue"
+read -p "Press enter to continue..."
 git add k8s/upgrades/README.md
 git commit -s -m "chore(upgrade): update upgrade README with 2.12.0 steps"
 git push origin docs-update
