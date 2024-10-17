238772b9aa5ab375a0ecc4a429c887a16e5c037f
Abhilash Shetty <abhilash.shetty@datacore.com>
2024-10-17 10:41:43 +0000

    feat(openebs_plugin): adding binary and initial implementation
    Signed-off-by: Abhilash Shetty <abhilash.shetty@datacore.com>

---
+++ plugin/src/cli_utils/clusterinfo/mod.rs
@@ -0,0 +1,40 @@
+use plugin::ExecuteOperation;
+use crate::cli_utils::CliArgs;
+
+#[derive(clap::Subcommand, Debug)]
+pub(crate) enum Operations {
+    // Gets informations of all installed engines
+    Get(ClusterInfoArgs)
+}
+
+#[async_trait::async_trait(?Send)]
+impl ExecuteOperation for Operations {
+    type Args = CliArgs;
+    type Error = Error;
+
+    async fn execute(&self, _cli_args: &CliArgs) -> Result<(), Error> {
+        match self {
+            Operations::Get(cluster_info_args) => {
+                println!("Clusterinfo cmd reached, {}", cluster_info_args.chart)
+            }
+        }
+        Ok(())
+    }
+}
+
+/// Arguments required when getting a cluster info
+#[derive(Debug, Clone, clap::Args)]
+pub struct ClusterInfoArgs {
+    /// Helm chart release name
+    chart: String
+}
+
+pub(crate) enum Error {
+    Generic(anyhow::Error),
+}
+
+impl From<anyhow::Error> for Error {
+    fn from(e: anyhow::Error) -> Self {
+        Error::Generic(e)
+    }
+}
