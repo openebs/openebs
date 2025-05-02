726c6e0741631b707f821d106028366d9e13af7a
Prateek Chandra <Prateek.Chandra@datacore.com>
2025-05-02 12:42:02 +0530

    fix: set mayastor submodule to the branch from chart version
    Signed-off-by: Prateek Chandra <Prateek.Chandra@datacore.com>

--- scripts/git/set-submodule-branches.sh
+++ scripts/git/set-submodule-branches.sh
@@ -2,6 +2,25 @@
 
 set -euo pipefail
 
+SCRIPT_DIR="$(dirname "$0")"
+ROOT_DIR="$(realpath $SCRIPT_DIR/../..)"
+CHART_DIR="$ROOT_DIR/charts"
+MAYASTOR_NAME="mayastor"
+
+echo_stderr() {
+  echo -e "${1}" >&2
+}
+
+die() {
+  local _return="${2:-1}"
+  echo_stderr "$1"
+  exit "${_return}"
+}
+
+mayastor_helm_version() {
+  helm show chart "$CHART_DIR" | dep="$MAYASTOR_NAME" yq '.dependencies[]|select(.name == strenv(dep)).version'
+}
+
 submodule_set_branch_all() {
   branch=${1:-}
   if [ -n "$branch" ]; then
@@ -12,6 +31,37 @@ submodule_set_branch_all() {
   for mod in `git config --file .gitmodules --get-regexp path | awk '{ print $2 }'`; do
     git submodule set-branch $branch $mod
   done
+}
+
+mayastor_branch_exists() {
+  local branch="$1"
+  cd "$ROOT_DIR/$MAYASTOR_NAME"
+  branch=$(git branch --list -r origin/$branch)
+  if [ $? -ne 0 ]; then
+    die "Failed to run Git branch command"
+  fi
+  [ -n "$branch" ]
+}
+
+mayastor_branch() {
+  version="$(mayastor_helm_version)"
+
+  local major="$(semver get major "$version")"
+  local minor="$(semver get minor "$version")"
+  local patch="$(semver get patch "$version")"
+  local prerel="$(semver get prerel "$version")"
+
+   if [[ "$version" == "0.0.0" ]] || [[ "$prerel" == "develop" ]]; then
+     BRANCH="develop"
+   else
+     BRANCH="release/${major}.${minor}"
+   fi
+
+   if mayastor_branch_exists "$BRANCH"; then
+     return 0
+   else
+     die "Cannot determine the correct $MAYASTOR_NAME branch!"
+   fi
 }
 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
@@ -25,17 +75,14 @@ while [ "$#" -gt 0 ]; do
       shift
       ;;
     -c|--clear)
-      CLEAR_BRANCH=="y"
+      CLEAR_BRANCH="y"
       shift
       ;;
     *)
-      echo "Unknown option: $1"
-      exit 1
+      die "Unknown option: $1"
       ;;
   esac
 done
-
-
 
 if [ "$BRANCH" == "develop" ] || [ "${BRANCH#release/}" != "${BRANCH}" ]; then
   SET_BRANCH="${BRANCH}"
@@ -46,5 +93,7 @@ if [ -n "$CLEAR_BRANCH" ]; then
 elif [ -n "$SET_BRANCH" ]; then
   submodule_set_branch_all "$SET_BRANCH"
 else
-  echo "Not modifying branch"
+  # If nothing is specified do it from the charts.
+  mayastor_branch
+  submodule_set_branch_all "$BRANCH"
 fi
