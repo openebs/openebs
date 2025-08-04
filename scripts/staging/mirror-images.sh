816228a78b32e0aded26bbadb3899cd78041e6e7
Abhinandan Purkait <purkaitabhinandan@gmail.com>
2025-08-04 13:47:06 +0000

    fix: staging shell, script and change namespaces
    Signed-off-by: Abhinandan Purkait <purkaitabhinandan@gmail.com>

--- scripts/staging/mirror-images.sh
+++ scripts/staging/mirror-images.sh
@@ -6,11 +6,12 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]:-"$0"}")")"
+CURRENT_ROOT_DIR="${SCRIPT_DIR}/../.."
 
 # if ROOT_DIR is not defined use the one below
-: "${ROOT_DIR:=${SCRIPT_DIR}/../..}"
+: "${ROOT_DIR:=$CURRENT_ROOT_DIR}"
 
-source "$ROOT_DIR/mayastor/scripts/utils/log.sh"
+source "$CURRENT_ROOT_DIR/mayastor/scripts/utils/log.sh"
 NO_RUN=true . "$ROOT_DIR/scripts/release.sh"
 
 IMAGES=()
--- scripts/staging/shell.nix
+++ scripts/staging/shell.nix
@@ -9,5 +9,12 @@ pkgs.mkShell {
     crane
     yq-go
     jq
+  ] ++ pkgs.lib.optional (builtins.getEnv "IN_NIX_SHELL" == "pure" && pkgs.system != "aarch64-darwin") [
+    docker
+    git
+    curl
+    nix
+    kubernetes-helm-wrapped
+    cacert
   ];
 }
